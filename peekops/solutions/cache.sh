55f34cd372e45e1bdba27658b97b1d5b79735592
Helmy Ligaputra <31847084+ligaputra@users.noreply.github.com>
2018-04-12 21:09:21 +0700

    Create cache.sh

---
+++ peekops/solutions/cache.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt-get install build-essential tcl
+curl -O http://download.redis.io/redis-stable.tar.gz
+tar xzvf redis-stable.tar.gz
+cd redis-stable
+make
+make test
+sudo make install
+sudo mkdir /etc/redis
+sudo cp /tmp/redis-stable/redis.conf /etc/redis
+sudo nano /etc/redis/redis.conf
