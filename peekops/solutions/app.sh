b35b6e04b1dd2d3251f7e26c2c9bbb669ca1eac3
Helmy Ligaputra <31847084+ligaputra@users.noreply.github.com>
2018-04-12 21:07:51 +0700

    Create app.sh

---
+++ peekops/solutions/app.sh
@@ -0,0 +1,8 @@
+sudo apt-get install curl git
+curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+nvm install 0.10.16
+npm install -g express
+mkdir socketio-test
+cd socketio-test
+express
+npm install
